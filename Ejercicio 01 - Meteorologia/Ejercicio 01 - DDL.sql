3c14a36ecd072790705becff79b8a1de0378b92d
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-31 17:53:21 +0200

    Ejercicio 01 añadido

---
+++ Ejercicio 01 - Meteorologia/Ejercicio 01 - DDL.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE meteorologia;
+USE meteorologia;
+
+CREATE TABLE estacion(
+	id int auto_increment,
+    latitud float,
+    longitud float,
+    altitud float,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE muestra(
+	id int auto_increment,
+    id_estacion int,
+    fecha date,
+    temp_min float,
+    temp_max float,
+    precipitaciones float,
+    humedad_min float,
+    humedad_max float,
+    vel_viento_max float,
+    vel_viento_min float,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_estacion) REFERENCES estacion (id)
+    ON DELETE cascade
+    ON UPDATE cascade
+);
